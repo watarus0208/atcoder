cc0571176d213cca34c3c2aa89da17ebe4cb4567
杉本亙 <sugimotowataru@sugimotowatarunoMacBook-Air.local>
2022-11-08 17:33:17 +0900

    フラグを立てる方法へ変更

--- abc118_b.py
+++ abc118_b.py
@@ -1,16 +1,7 @@
 N, M = map(int, input().split())
-common = []
-for i in range(N):
-    if i==0:
-        common = list(map(int, input().split()))[1:]
-        print('i==0')
-        print(common)
-    else:
-        tmp = list(map(int, input().split()))[1:]
-        for j, c in enumerate(common):
-            if c not in tmp:
-                common.pop(j)
-                print(i)
-                print(common)
-
-print(len(common))
+fav = [0]*M
+for _ in range(N):
+    KA = list(map(int, input().split()))
+    for a in range(KA[0]):
+        fav[KA[a+1]-1] += 1
+print(fav)
